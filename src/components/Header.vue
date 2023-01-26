a13a9b24cef06aa318bfa0d03147b38d73f0a294
Rafael Lazarini <rlazarini@gmail.com>
2023-01-26 14:56:51 -0300

    feat(header): End basic layout to header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,13 +1,13 @@
 <template>
   <header class="bg-white">
     <nav class="relative px-4 py-4 flex justify-start items-center bg-cyan-200">
-      <div class="">
+      <div class="mr-1.5">
         <button
-          class="navbar-burger flex items-center text-black p-3"
+          class="navbar-burger flex items-center text-black p-2"
           @click="toggleMenu"
         >
           <svg
-            class="block h-4 w-4 fill-current"
+            class="block h-6 w-4 fill-current"
             viewBox="0 0 20 20"
             xmlns="http://www.w3.org/2000/svg"
           >
@@ -16,12 +16,55 @@
         </button>
       </div>
       <a
-        class="text-3xl font-bold leading-none"
+        class="text-3xl font-bold leading-none flex items-center"
         href="#"
       >
         <img
           src="cart-logo.png"
           alt="E-commerce Template"
+          class="h-10"
+        >
+        <h1 class="text-xl text-gray-500">{{ title }}</h1>
+      </a>
+      <form class="ml-auto mr-6 w-1/2 mt-1">
+        <div class="relative">
+          <input
+            type="search"
+            id="product-search"
+            class="block outline-none w-full p-2 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Buscar produtos..."
+            required
+          >
+          <button
+            type="submit"
+            class="absolute inset-y-0 right-4 flex items-center pl-3 pointer-events-none"
+          >
+            <svg
+              aria-hidden="true"
+              class="w-5 h-5 text-gray-500"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </form>
+      <a
+        class="text-3xl font-bold leading-none items-top"
+        title="Sacola de compras"
+        href="#"
+      >
+        <img
+          src="checkout.png"
+          alt="Sacola"
           class="h-10"
         >
       </a>
@@ -90,7 +133,6 @@
         </div>
       </nav>
     </div>
-    Logo LOLs - {{ title }}
   </header>
 </template>
   
