c01ec0fd515877d3f056db1df1d04081e40bc591
Rafael Lazarini <rlazarini@gmail.com>
2023-01-31 22:24:56 -0300

    feat(cart): Show dropdown with items add in shopcart

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -85,26 +85,29 @@
             "
           v-if="cart.length"
         >
-          {{ cart.length }}
+          {{ cart.map(e => e.quantity || 1).reduce((a,b) => a + b) }}
         </span>
 
-        <div class="absolute w-full hidden group-hover:block rounded-b border-t-0 z-10 right-0 w-auto">
-          <div class="shadow-xl w-64">
+        <div class="absolute w-full hidden group-hover:block rounded-b border-t-0 z-10 right-0 w-auto bg-whtie z-50">
+          <div class="shadow-xl w-72 rounded-lg bg-white">
             <div
               class="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100"
-              style=""
+              v-for="(item, index) in cart"
+              :key="index"
             >
-              <div class="p-2 w-12"><img
-                  src="https://dummyimage.com/50x50/bababa/0011ff&amp;text=50x50"
-                  alt="img product"
-                ></div>
+              <div class="p-2 w-12">
+                <img :src="item.image">
+              </div>
               <div class="flex-auto text-sm w-32">
-                <div class="font-bold">Product 1</div>
-                <div class="truncate">Product 1 description</div>
-                <div class="text-gray-400">Qt: 2</div>
+                <div class="font-bold line-clamp-2 h-11">{{item.title}}</div>
+                <div class="text-sm font-normal text-gray-400">Qt: {{item.quantity || 1}}</div>
               </div>
               <div class="flex flex-col w-18 font-medium items-end">
-                <div class="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
+                <div
+                  title="Remover"
+                  class="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700"
+                  @click="removeProductCart(item)"
+                >
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     width="100%"
@@ -115,7 +118,7 @@
                     stroke-width="2"
                     stroke-linecap="round"
                     stroke-linejoin="round"
-                    class="feather feather-trash-2 "
+                    class="feather feather-trash-2"
                   >
                     <polyline points="3 6 5 6 21 6"></polyline>
                     <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
@@ -134,110 +137,40 @@
                     ></line>
                   </svg>
                 </div>
-                $12.22
-              </div>
-            </div>
-            <div
-              class="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100"
-              style=""
-            >
-              <div class="p-2 w-12"><img
-                  src="https://dummyimage.com/50x50/bababa/0011ff&amp;text=50x50"
-                  alt="img product"
-                ></div>
-              <div class="flex-auto text-sm w-32">
-                <div class="font-bold">Product 2</div>
-                <div class="truncate">Product 2 long description</div>
-                <div class="text-gray-400">Qt: 2</div>
-              </div>
-              <div class="flex flex-col w-18 font-medium items-end">
-                <div class="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="100%"
-                    height="100%"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-trash-2 "
-                  >
-                    <polyline points="3 6 5 6 21 6"></polyline>
-                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                    </path>
-                    <line
-                      x1="10"
-                      y1="11"
-                      x2="10"
-                      y2="17"
-                    ></line>
-                    <line
-                      x1="14"
-                      y1="11"
-                      x2="14"
-                      y2="17"
-                    ></line>
-                  </svg>
-                </div>
-                $12.22
-              </div>
-            </div>
-            <div
-              class="p-2 flex bg-white hover:bg-gray-100 cursor-pointer border-b border-gray-100"
-              style=""
-            >
-              <div class="p-2 w-12"><img
-                  src="https://dummyimage.com/50x50/bababa/0011ff&amp;text=50x50"
-                  alt="img product"
-                ></div>
-              <div class="flex-auto text-sm w-32">
-                <div class="font-bold">Product 3</div>
-                <div class="truncate">Product 3 description</div>
-                <div class="text-gray-400">Qt: 2</div>
-              </div>
-              <div class="flex flex-col w-18 font-medium items-end">
-                <div class="w-4 h-4 mb-6 hover:bg-red-200 rounded-full cursor-pointer text-red-700">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="100%"
-                    height="100%"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                    stroke-width="2"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    class="feather feather-trash-2 "
-                  >
-                    <polyline points="3 6 5 6 21 6"></polyline>
-                    <path d="M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2">
-                    </path>
-                    <line
-                      x1="10"
-                      y1="11"
-                      x2="10"
-                      y2="17"
-                    ></line>
-                    <line
-                      x1="14"
-                      y1="11"
-                      x2="14"
-                      y2="17"
-                    ></line>
-                  </svg>
-                </div>
-                $12.22
+                <span
+                  class="text-sm"
+                  :title="formatPrice(item.price * (item.quantity || 1))"
+                >
+                  {{ formatPrice(item.price * (item.quantity || 1)) }}
+                </span>
               </div>
             </div>
             <div class="p-4 justify-center flex">
-              <button class="text-base  undefined  hover:scale-110 focus:outline-none flex justify-center px-4 py-2 rounded font-bold cursor-pointer 
-                        hover:bg-teal-700 hover:text-teal-100 
-                        bg-teal-100 
-                        text-teal-700 
-                        border duration-200 ease-in-out 
-                        border-teal-600 transition">Checkout $36.66</button>
+              <button
+                class="
+                text-base 
+                focus:outline-none
+                flex
+                justify-center
+                px-4
+                py-2
+                rounded
+                font-bold
+                cursor-pointer 
+                hover:bg-[#FCA3CC]
+                hover:text-gray-100
+                bg-[#FBBEDF] 
+                text-gray-700
+                border
+                duration-200
+                ease-in-out 
+                border-gray-400	
+                transition
+                "
+                :title="'Comprar ' + formatPrice(state.price)"
+              >
+                Comprar {{ formatPrice(state.price) }}
+              </button>
             </div>
           </div>
         </div>
@@ -307,7 +240,7 @@
 </template>
   
   <script>
-import { reactive, watch, computed, onMounted } from "vue";
+import { reactive, ref, computed, onMounted } from "vue";
 import { useStore } from "vuex";
 
 export default {
@@ -318,9 +251,19 @@ export default {
     const store = useStore();
     const state = reactive({
       showMenu: true,
+      lang: navigator.language,
+      price: ref(0),
     });
-    const cart = computed(() => store.getters.getCart);
-
+    const cart = computed(() => {
+      const cartObj = store.getters.getCart;
+      state.price = cartObj
+        .map((e) => e.price * (e.quantity || 1))
+        .reduce((a, b) => a + b);
+      return cartObj;
+    });
+    const removeProductCart = (product) => {
+      store.dispatch("removeProductCart", product);
+    };
     const toggleMenu = () => {
       state.showMenu = !state.showMenu;
     };
@@ -328,11 +271,28 @@ export default {
       return store.getters.getCategories;
     });
 
+    const formatPrice = (price) => {
+      return price.toLocaleString(state.lang, {
+        style: "currency",
+        currency: "BRL",
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+      });
+    };
+
     onMounted(async () => {
       await store.dispatch("fetchCategories");
     });
 
-    return { props, state, cart, getCategories, toggleMenu };
+    return {
+      props,
+      state,
+      cart,
+      formatPrice,
+      removeProductCart,
+      getCategories,
+      toggleMenu,
+    };
   },
 };
 </script>
